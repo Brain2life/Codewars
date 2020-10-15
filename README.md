d0570f7b496c2166c31d29ae728519e60238e5e2
brain2life <maxat.akbanov@gmail.com>
2020-10-15 17:33:14 +0600

    init repo

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository represents programming exercises solved from Codewars [platform](https://www.codewars.com/).
---
+++ rot13.py
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/530e15517bc88ac656000716/train/python
+# Implement ROT13 simple letter substitution cipher
+
+def rot13(message):
+    
